a499bfdd4cd2fcdde6b3a01cdb65e044866d3349
João Manoel Neto <joaaomanooel@gmail.com>
2017-06-21 14:19:16 -0300

    Adiconar front do download da Udamy

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,6 @@
 module.exports = function(application){
     application.get('/', function(req, res){
-            res.send('Olá João Manoel');
+            //res.send('Olá João Manoel');
+            res.render('index');
     });
 }
