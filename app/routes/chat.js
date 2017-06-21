e672d4030c504ea721e47d9ea190d58891268957
João Manoel Neto <joaaomanooel@gmail.com>
2017-06-21 15:44:04 -0300

    ajustando rotas e validando form da index

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -2,10 +2,8 @@ module.exports = function(application){
     application.post('/chat', function(req, res){
             application.app.controllers.chat.iniciaChat(application, req, res);
     });
-}
 
-module.exports = function(application){
-    application.get('/chat', function(req, res){
+        application.get('/chat', function(req, res){
             application.app.controllers.chat.iniciaChat(application, req, res);
     });
 }
