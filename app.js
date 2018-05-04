27599b58b8ab60440c54eadd650ebb8a45b01a32
João Manoel Neto <joaaomanooel@gmail.com>
2018-05-04 00:44:06 -0300

    refactoring server.js and app.js for es6

--- app.js
+++ app.js
@@ -1,45 +1,42 @@
-/* importar as configurações do servidor  */
-var app = require('./config/server');
+import listen from 'socket.io';
+import app from './config/server';
 
-/* parametrizar a porta de escuta */
-var server = app.listen(7000, function(){
-    console.log('Server on');
-    console.log('Port: 7000');
-    console.log('URL: http://localhost:7000');
-})
+const server = app.listen(7000, () => {
+  console.log(`
+---> Server on
+---> Port: 7000
+---> URL: http://localhost:7000
+  `);
+});
 
-var io = require('socket.io').listen(server);
-app.set('io', io); //<- Variavel global passada a partir da app
-/**
- * criar a conexão por websocket
- */
-io.on('connection', function(socket){
-    socket.on('msgParaServidor', function(data){
-        /**
-         * Dialogo
-         */
-        socket.emit(
-            'msgParaClientes',
-            {apelido : data.apelido, msg: data.msg}    
-        );
-    
-        socket.broadcast.emit(
-            'msgParaClientes',
-            {apelido : data.apelido, msg: data.msg}    
-        );
-        /**
-         * Participantes
-         */
-        if(parseInt(data.apelido_atualizado) == 0){
-            socket.emit(
-                'participantesParaCliente',
-                {apelido : data.apelido}    
-            );
-    
-            socket.broadcast.emit(
-                'participantesParaCliente',
-                {apelido : data.apelido}    
-            );
-        }
-    });
+const io = listen(server);
+app.set('io', io);
+io.on('connection', (socket) => {
+  socket.on('msgParaServidor', (data) => {
+    socket.emit(
+      'msgParaClientes',
+      {
+        apelido: data.apelido,
+        msg: data.msg
+      }
+    );
+    socket.broadcast.emit(
+      'msgParaClientes',
+      {
+        apelido: data.apelido,
+        msg: data.msg
+      }
+    );
+    const apelidoAtualizado = parseInt(data.apelido_atualizado, 10);
+    if (apelidoAtualizado === 0) {
+      socket.emit(
+        'participantesParaCliente',
+        { apelido: data.apelido }
+      );
+      socket.broadcast.emit(
+        'participantesParaCliente',
+        { apelido: data.apelido }
+      );
+    }
+  });
 });
